2d32213f2f4e5a9f99ad17181585605ad769ead2
brickfx <252142844@qq.com>
2015-04-27 00:37:56 +0800

    完成基本框架搭建 	修改:         .idea/workspace.xml 	修改:         User/static/css/index.css 	新文件:       User/templates/User/download.html 	修改:         User/templates/User/index.html 	新文件:       User/templates/User/index2.html 	新文件:       User/templates/User/list.html 	新文件:       User/templates/User/share.html 	新文件:       User/templates/User/upload.html 	修改:         User/urls.py 	修改:         User/urls.pyc 	修改:         User/views.py 	修改:         User/views.pyc

--- User/urls.py
+++ User/urls.py
@@ -4,4 +4,8 @@ from User.views import *
 urlpatterns = patterns('',
                        url('^login', login, name="login"),
                        url('^index', index, name="index"),
+                       url('^upload/$', upload_file, name="upload"),
+                       url('^download/$', download_file, name="download"),
+                       url('^share/$', share_file, name='share'),
+                       url('^list/$', list_file, name='list'),
                        )
--- User/views.py
+++ User/views.py
@@ -9,3 +9,15 @@ def login(request):
 
 def index(request):
     return render(request, 'User/index.html', {})
+
+def upload_file(request):
+    return render(request, 'User/upload.html', {})
+
+def download_file(request):
+    return render(request, 'User/download.html', {})
+
+def share_file(request):
+    return render(request, 'User/share.html', {})
+
+def list_file(request):
+    return render(request, 'User/list.html', {})
