fc87b60f27f1d0d26afca5bb15bc2b6cffc8b462
brickfx <252142844@qq.com>
2015-05-02 21:58:29 +0800

    用户自定义User需要删掉所有migrations，然后再同步数据库 	修改:         .idea/workspace.xml 	修改:         User/migrations/0001_initial.py 	修改:         User/migrations/0001_initial.pyc 	删除:         User/migrations/0002_delete_myuser.py 	删除:         User/migrations/0002_delete_myuser.pyc 	删除:         User/migrations/0003_myuser.py 	删除:         User/migrations/0003_myuser.pyc 	删除:         User/migrations/0004_delete_myuser.py 	删除:         User/migrations/0004_delete_myuser.pyc 	删除:         User/migrations/0005_myuser.py 	删除:         User/migrations/0005_myuser.pyc 	删除:         User/migrations/0006_delete_myuser.py

--- User/migrations/0001_initial.py
+++ User/migrations/0001_initial.py
@@ -17,7 +17,11 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('password', models.CharField(max_length=128, verbose_name='password')),
                 ('last_login', models.DateTimeField(default=django.utils.timezone.now, verbose_name='last login')),
-                ('identifier', models.EmailField(unique=True, max_length=75)),
+                ('email', models.EmailField(unique=True, max_length=255, verbose_name=b'email address')),
+                ('username', models.CharField(max_length=30)),
+                ('date_of_birth', models.DateField()),
+                ('is_active', models.BooleanField(default=True)),
+                ('is_admin', models.BooleanField(default=False)),
             ],
             options={
                 'abstract': False,
