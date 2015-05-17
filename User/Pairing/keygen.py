b20dc8d84f3cb437e6f8753d90d295e04d819ddb
brickfx <252142844@qq.com>
2015-05-17 19:54:46 +0800

    refomart thirty party code

--- User/Pairing/keygen.py
+++ User/Pairing/keygen.py
@@ -1,18 +1,19 @@
 from math import fmod
 from random import randint
-from tree import tree , keyTree
+from tree import tree, keyTree
 import gPair
 
+
 class KeyGene:
-	masterKey = []
+    masterKey = []
 
-	def __init__(self,masterKey):
-		self.masterKey = masterKey
+    def __init__(self, masterKey):
+        self.masterKey = masterKey
 
-	def keygen(self,albero):
-		alberochiavi = keyTree()
-		alberochiavi.generaFunzioni(albero, self.masterKey[-1])
-		foglie = alberochiavi.estraiFoglie()
-		print foglie
-		key =[(x[0] ,gPair.pot(gPair.radice(gPair.g , self.masterKey[x[0]])  , y)) for x,y in foglie ]
-		return key
+    def keygen(self, albero):
+        alberochiavi = keyTree()
+        alberochiavi.generaFunzioni(albero, self.masterKey[-1])
+        foglie = alberochiavi.estraiFoglie()
+        print foglie
+        key = [(x[0], gPair.pot(gPair.radice(gPair.g, self.masterKey[x[0]]), y)) for x, y in foglie]
+        return key
--- User/Pairing/test.py
+++ User/Pairing/test.py
@@ -11,28 +11,28 @@ cry = Crypto(3)
 # 	tree(1,[3],[]) 
 # 	])
 
-alb1 = tree(2,[0,1,2],[
-	tree(1,[0],[]),
-	tree(1,[2],[])
-	])
+alb1 = tree(2, [0, 1, 2], [
+    tree(1, [0], []),
+    tree(1, [2], [])
+])
 
-att = [0,2]
+att = [0, 2]
 mess = 100
 
 
 print 'cry.publickey',  cry.publicKey
 print "Public Key : {}".format(cry.publicKey)
 print "Master Key : {}".format(cry.masterKey)
-E = cry.encrypto(mess,att)
+E = cry.encrypto(mess, att)
 print "message encrypted", E
 
 print "Ciphertext : {}".format(E), '---'
 
 privatekey1 = cry.keygenerator.keygen(alb1)
 
-print "Private Key {} \n for {} ".format(privatekey1,alb1)
+print "Private Key {} \n for {} ".format(privatekey1, alb1)
 
 decry = deCrypto(alb1,privatekey1)
 print "decry---------------------", decry
 
-print "Mess: {}".format(decry.decifra(E,privatekey1))
+print "Mess: {}".format(decry.decifra(E, privatekey1))
