a99f79258c66ef7e1fa36f614c839430f11404b2
brickfx <252142844@qq.com>
2015-06-01 16:38:16 +0800

    a small chage to admin page

--- ABE/settings.py
+++ ABE/settings.py
@@ -83,7 +83,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
